3ab0f3ae8648727ee47e1f17a01ddfdb286e1a2d
Logan Richardson <reviewedge56@gmail.com>
2022-10-22 10:27:05 -0400

    make it so robotGuard can die

--- Assets/Scripts/RobotGuardScript.cs
+++ Assets/Scripts/RobotGuardScript.cs
@@ -6,21 +6,41 @@ using UnityEngine;
 public class RobotGuardScript : MonoBehaviour
 {
     Rigidbody2D _rbody;
+
+    public bool _isDead;
+    public int _health;
+
     public float _speed;
     public GameObject _player;
+    public Sprite _weaponDrop;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _isDead = false;
+
+        _player = GameObject.FindWithTag(TagList.playerTag);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Transform target = _player.transform;
+        if (!_isDead)
+        {
+            Transform target = _player.transform;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
 
+            if (_health <= 0)
+            {
+                Die();
+            }
+        }
+    }
 
-        transform.position = Vector2.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
+    void Die()
+    {
+        _isDead = true;
+        gameObject.GetComponent<SpriteRenderer>().sprite = _weaponDrop;
     }
 }
