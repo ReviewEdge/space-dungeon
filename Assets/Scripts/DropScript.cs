d4c6648403302c292c8044a1dbe34776b37a10c5
Logan Richardson <reviewedge56@gmail.com>
2022-11-01 15:01:14 -0400

    stop popup text from spinning with drop

--- Assets/Scripts/DropScript.cs
+++ Assets/Scripts/DropScript.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class DropScript : MonoBehaviour
 {
     private MeshRenderer _popupText;
+    GameObject _popupChild;
     // Start is called before the first frame update
     void Start()
     {
         _popupText = GetComponentInChildren<MeshRenderer>();
+        _popupChild = gameObject.transform.Find("PopupText").gameObject;
     }
 
     // Update is called once per frame
@@ -16,7 +18,16 @@ public class DropScript : MonoBehaviour
     {
         // spinny spinny
         gameObject.transform.Rotate( 0, Time.deltaTime*96f, 0);
+        
+        // stops popup text from spinning
+        _popupChild.transform.Rotate(0, -Time.deltaTime*96f, 0);
     }
+
+    public void LateUpdate() {
+        // stops popup text from spinning
+        _popupChild.transform.position = gameObject.transform.position;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == TagList.playerTag)
