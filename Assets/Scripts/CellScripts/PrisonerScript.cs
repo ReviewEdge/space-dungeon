4324fe62282a6ff4db9820d1b96f439a5c400ad8
Tjrunner1 <115499975+Tjrunner1@users.noreply.github.com>
2022-11-02 11:46:34 -0400

    Made it so the UI screen is scalable

--- Assets/Scripts/CellScripts/PrisonerScript.cs
+++ Assets/Scripts/CellScripts/PrisonerScript.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class PrisonerScript : MonoBehaviour
 {
     private SpriteRenderer _spriteRenderer;
+    private Rigidbody2D _rbody;
+    private int x = 1;
+    private int y = 0;
+    public LayerMask layerMask;
     // Start is called before the first frame update
     void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _rbody.velocity = new Vector2(x, y);
+        RaycastHit2D findWall = Physics2D.Raycast(_rbody.position, Vector2.right * x, .001f, layerMask);
+        if (findWall.collider != null) {
+            x = x * -1;
+        }
     }
 
     public void Freed() {
